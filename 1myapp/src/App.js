a0997b0967159bfd2ef3f1ea2673d7e3c367cf16
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-18 00:32:29 +0530

    screens and passed in app.js

---
+++ 1myapp/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import _1About from './screens/_1About';
+import _2Contact from './screens/_2Contact';
+
+function App() {
+  return (
+    // <h1>Hi</h1>   
+    // <_1About /> 
+    <div>
+      <_1About />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <_2Contact />
+    </div>
+
+
+  );
+}
+
+export default App; 
+
+
+
+// return always give only one element,if want to return more then wrap in div
