59e86d94ec972c1c0258303cbbd9bd220494ec4b
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-20 01:50:08 +0530

    UseEffect

--- 9useeffect/src/App.js
+++ 9useeffect/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Useeffect1 from "./screens/Useeffect1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <p>UseEffect understand</p>
+    <p>..............................................................................</p>
+    <Useeffect1 />
+   </div>
   );
 }
 
---
+++ 9useeffect/src/screens/Useeffect1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Useeffect1() {
+  return (
+    <div>
+      <p>Useeffect1</p>
+      <p>.................................................................................</p>
+    </div>
+  )
+}
+
+export default Useeffect1
