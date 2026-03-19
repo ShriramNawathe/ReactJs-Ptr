0755b3f897bfe091eff7df0b885553aab8e3f4c4
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-20 01:04:21 +0530

    Hook UseState

---
+++ 8usestate/src/App.js
@@ -0,0 +1,11 @@
+import Usestate1 from "./screens/Usestate1";
+
+function App() {
+  return (
+  <div>hi
+    <Usestate1/>
+  </div>
+  );
+}
+
+export default App;
---
+++ 8usestate/src/screens/Usestate1.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function Usestate1() {
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //once you click button react reloads page automatically, autoreload stop
+    console.log("submit clicked");
+  }
+  //get exact values when we type in input email, setEmail updates and store in variable email
+     const [email, setEmail] = useState();
+  // const [password,setPassword]=useState();
+
+
+console.log(email);
+  return (
+    <div>
+      <p>Usestate1</p>
+      <p>............................................................</p>
+      <br />
+      <form>
+        <input 
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}//understand via chatgpt
+        />
+        <br />
+        {/* <input type="email" placeholder="Enter Password" value={email}/>  */} 
+        {/* get exact values when we type in input email, setEmail updates and store in variable email */}
+        <input type="email" placeholder="Enter Password" />
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Usestate1;
