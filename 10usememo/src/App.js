9b7f5bba873ed197b670c029795183db808670ec
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-20 21:06:48 +0530

    useMemo Start

---
+++ 10usememo/src/App.js
@@ -0,0 +1,14 @@
+import Usememo from "./screens/Usememo";
+
+
+function App() {
+  return (
+    <div>
+      <Usememo/>
+      <p>........................................</p>
+      <br/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 10usememo/src/screens/Usememo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Usememo() {
+  return (
+    <div>
+        <p>Usememo</p>
+        <p>....................................................................1</p>
+    </div>
+  )
+}
+
+export default Usememo
