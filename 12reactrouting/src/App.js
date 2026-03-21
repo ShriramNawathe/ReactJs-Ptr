c93aab481562875b74dcb9cdb64e105d90f52b1b
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-21 19:29:20 +0530

    react router path app.js

--- 12reactrouting/src/App.js
+++ 12reactrouting/src/App.js
@@ -1,8 +1,37 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from '../src/screens/Home'
+import Aboutus from '../src/screens/Aboutus'
+import Links from '../src/screens/Links'
+import Registor from '../src/screens/Register'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>
+  },
+  {
+    path: "/Home",
+    element: <Home/>
+  },
+  {
+    path: "/Aboutus",
+    element: <Aboutus/>
+  },
+  {
+    path: "/Links",
+    element: <Links/>
+  },
+  {
+    path: "/Registor",
+    element: <Registor/>
+  },
+]);
 
 function App() {
   return (
     <div>
-      hi
+       <RouterProvider router={router} />,
     </div>
   );
 }
