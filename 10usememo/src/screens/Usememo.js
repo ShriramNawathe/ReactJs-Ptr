6edaaffc763acdadea84ce4e39b059ec06f7ba08
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-21 18:35:29 +0530

    useMemo

--- 10usememo/src/screens/Usememo.js
+++ 10usememo/src/screens/Usememo.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
+
+const expensiveCalculation=(num)=>{
+  console.log("Calculating...........");
+  for (let i=0;i<1000000000;i++){
+    num=num+1;
+  }
+  return num;
+}
+
 
 function Usememo() {
+  const[todos,setTodos]=useState([]);
+  const addTodos=()=>{
+    // setTodos((t)=>t+1);
+    // setTodos((t)=>[...t,"Adding todos after button click into the useState([]) array. Useing spread operator///// this is string"]);
+   setTodos((t)=>[...t,"Todo TTasks"])
+  }
+
+  const[count,setCount]=useState(0);
+  const Inc=()=>{
+    setCount(count+1);
+  }
+
+  // it takes delay through out code 2-3sec
+  // const calculation=expensiveCalculation(count);
+  //after this line throughout code all code takes 2-3 sec delay both adding todo and in counter as well
+  const calculation=useMemo(()=>expensiveCalculation(count),[count])
   return (
     <div>
         <p>Usememo</p>
         <p>....................................................................1</p>
+        <div>
+          <div>
+            <h1>Todo list</h1>
+            {/* {todos} //here what happen is todo after 1 todo after another todo like all todos in same line */}
+            {/* adding todo in seperate elements */}
+            {todos.map((todo,index)=>{
+              return (
+                <p key={index}>{todo}</p>
+              )
+            })}
+            <br/>
+            <button onClick={addTodos}>AddTodos</button>
+          </div>
+        </div>
+        <hr/>
+        <div>
+          <h1>Count:{count}</h1>
+          <br/>
+          <button onClick={Inc}>+</button>
+          <br/>
+          {/* till here code run smooth afte this takes delay for 2-3sec */}
+          <h2>Expensive Calculation</h2>
+          {calculation}
+        </div>
     </div>
   )
 }
